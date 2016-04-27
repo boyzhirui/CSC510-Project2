0f5b3040520e7d30c576988e3f5acb9067ee356f
nikign <niki.hp2007@gmail.com>
2016-04-26 22:37:51 -0400

    included comment stuff

--- gitable.py
+++ gitable.py
@@ -58,7 +58,7 @@ dates = {}
 prev_data = None
 def dump_commits(u,commits):
     global dates, prev_data
-    token = "fdc02bd8ccae8c5edd61d9e6274487db0490926c" # <===
+    token = "cda87dde4e88524c3d614ba984f42621b2b13194" # <===
     request = urllib2.Request(u, headers={"Authorization" : "token "+token})
     v = urllib2.urlopen(request).read()
     w = json.loads(v)
@@ -74,6 +74,25 @@ def dump_commits(u,commits):
         if date.day not in dates[date.month]:
             dates[date.month][date.day] = ['%s/%s' %(date.month, date.day), 0]
         dates[date.month][date.day][1] += 1
+    return True
+
+users = {}
+def dump_comments(u):
+    global dates, prev_data, users
+    token = "cda87dde4e88524c3d614ba984f42621b2b13194" # <===
+    request = urllib2.Request(u, headers={"Authorization" : "token "+token})
+    v = urllib2.urlopen(request).read()
+    w = json.loads(v)
+    if not w: return False
+    for comment in w:
+        user = comment['user']['id']
+        if user not in users:
+            print('%s not found ' %comment['user']['login'])
+            continue
+        # if user not in users:
+        #     users[user] = 0
+        users[user] += 1
+    # print(str(users))
     return True
     
 
@@ -111,15 +130,15 @@ def dump(u,issues):
         print("Contact TA")
         return False
 import copy
-def launchDump():
+def dumpCommits():
     info = {}
     global dates
-    # links_to_repo = ['/nikign/Git-Helper']
-    links_to_repo = ['/Arjun-Code-Knight/csc510-se-project', '/ankitkumar93/csc510-se-project',
-        '/azhe825/CSC510', '/jordy-jose/CSC_510_group_d', '/DharmendraVaghela/csc510-grp-e',
-        '/moharnab123saikia/CSC510-group-f', '/cleebp/csc-510-group-g', '/nikign/SE-16', '/shivamgulati1991/CSC510-SE-group-i',
-        '/arnabsaha1011/mypackse', '/alokrk/csc510groupk', '/sandz-in/csc510_group_l',
-        '/nikraina/CSC510', '/gvivek19/CSC510-Team-N'] 
+    links_to_repo = ['/nikign/Git-Helper']
+    # links_to_repo = ['/Arjun-Code-Knight/csc510-se-project', '/ankitkumar93/csc510-se-project',
+    #     '/azhe825/CSC510', '/jordy-jose/CSC_510_group_d', '/DharmendraVaghela/csc510-grp-e',
+    #     '/moharnab123saikia/CSC510-group-f', '/cleebp/csc-510-group-g', '/nikign/SE-16', '/shivamgulati1991/CSC510-SE-group-i',
+    #     '/arnabsaha1011/mypackse', '/alokrk/csc510groupk', '/sandz-in/csc510_group_l',
+    #     '/nikraina/CSC510', '/gvivek19/CSC510-Team-N'] 
         
     for link in links_to_repo:
         page = 1
@@ -127,27 +146,109 @@ def launchDump():
         print ('link: %s' % link)
         dates = {}
         while(True):
-            # use for time  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1454632798.0))
-        # address = 'https://api.github.com/%s/graphs/contributors' % (link)
             address = 'https://api.github.com/repos%s/commits?page=%d' % (link, page)
-            # address = 'https://api.github.com/repos%s/issues/events?page=%d' % (link, page)
             doNext = dump_commits(address, issues)
-            # print(doNext)
-            # print("page "+ str(page))
             page += 1
             if not doNext : break
         for k in dates.keys():
-            # print(k)
             for kk in dates[k].keys():
                 print('%s\t%d' %(dates[k][kk][0], dates[k][kk][1]))
-        # info[link] = copy.deepcopy(issues)
-        # for issue, events in issues.iteritems():
-        #     print("ISSUE " + str(issue))
-        #     for event in events: print(event.show())
-        #     print('')
-        # print(str(info[link]))
+
+# from statistics import sdev, mean
+import numpy
+def find_outlier_groups(groups):
+       
+    # outlier of sdevs
+    groups_sdev = [i[0] for i in groups.values()]
+    groups_sum = [i[1] for i in groups.values()]
+
+    sum_sdev = numpy.std(numpy.array(groups_sum))
+    sum_mean = numpy.mean(numpy.array(groups_sum))
+
+    total_sdev = numpy.std(numpy.array(groups_sdev))
+    total_mean = numpy.mean(numpy.array(groups_sdev))
+    for group in groups.keys():
+        group_sum_score = (groups[group][1] - sum_mean)/(1.0*sum_sdev)
+        group_sdev_score = (groups[group][0] - total_mean)/(1.0*total_sdev)
+        print('group: %s, sdev score: %f, sum score: %f' %(group, group_sdev_score, group_sum_score))
+        if group_sdev_score > 1.5 or group_sdev_score < -1.5:
+            print('Group with much sdev: %s' %group)
+        if group_sum_score < -1.5:
+            print('Group with low comments: %s' %group)
+
+def fill_group_users(link):
+    global users
+    address = 'https://api.github.com/repos%s/stats/contributors' % link
+    # print(address)
+    # /repos/:owner/:repo/collaborators
+    token = "cda87dde4e88524c3d614ba984f42621b2b13194" # <===
+    request = urllib2.Request(address, headers={"Authorization" : "token "+token})
+    v = urllib2.urlopen(request).read()
+    w = json.loads(v)
+    if not w: return False
+    user_ids = [ww['author']['id'] for ww in w]
+    for user_id in user_ids:
+        users[user_id] = 0
+
+    # print(user_ids)
+    # for user_ids
+    
+            
+
+
+
+def dumpComments():
+    info = {}
+    global dates, users
+    # links_to_repo = ['/ankitkumar93/csc510-se-project']
+    links_to_repo = ['/Arjun-Code-Knight/csc510-se-project', '/ankitkumar93/csc510-se-project',
+        '/azhe825/CSC510', '/jordy-jose/CSC_510_group_d', '/DharmendraVaghela/csc510-grp-e',
+        '/moharnab123saikia/CSC510-group-f', '/cleebp/csc-510-group-g', '/nikign/SE-16', '/shivamgulati1991/CSC510-SE-group-i',
+        '/arnabsaha1011/mypackse', '/alokrk/csc510groupk', '/sandz-in/csc510_group_l',
+        '/nikraina/CSC510', '/gvivek19/CSC510-Team-N'] 
         
-launchDump()
+    groups = {}
+    for link in links_to_repo:
+        page = 1
+        users = {}
+        fill_group_users(link)
+        while(True):
+            # address = 'https://api.github.com/repos%s/commits?page=%d' % (link, page)
+            # doNext = dump_commits(address, issues)
+            # dates = {}
+            address = 'https://api.github.com/repos%s/issues/comments?page=%d' % (link, page)
+            doNext = dump_comments(address)
+            print('page: %d' % page)
+            page += 1
+            if not doNext : break
+            issues = dict()
+        print ('link: %s' % link)
+        for user in users.keys():
+            print('%s\t%d' %(user, users[user]))
+
+        mean = numpy.mean(users.values())
+        sdev = numpy.std(users.values())
+
+        scores = [(user - mean)/(1.0*sdev) for user in users.values()]
+        bad_smell = 0
+        for score in scores:
+            if score < -1.5 or score > 1.5:
+                bad_smell = 1
+                break
+        # if bad_smell:
+        #     print('Group %s has bad smell' % link)
+        groups[link] = (bad_smell, sum(users.values())*1.0/len(users.keys()))
+    # find_outlier_groups(groups)
+    max_sum = max([group[1] for group in groups.values()])
+    print("group name\toutlier in group comments\ttotal comments")
+    for link in groups:
+        bad_smell2 = 1 - (groups[link][1]*1.0)/max_sum
+        # groups[link][1] = bad_smell2
+        print("%s\t%f\t%f" %(link, groups[link][0], bad_smell2))
+
+    
+    
+dumpComments()
 
 
     
